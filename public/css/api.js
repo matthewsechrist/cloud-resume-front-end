c776dee1a3774a1c62cd4d159fb784615bf97903
MattSechrist <80978245+MattSechrist@users.noreply.github.com>
2021-09-06 07:47:02 -0400

    Update api.js

--- public/css/api.js
+++ public/css/api.js
@@ -4,7 +4,7 @@ const getVisitorCount = async () => {
     const response = await fetch("https://ivinlkop0h.execute-api.us-east-1.amazonaws.com/default/visitors”)
     const data = await response.json()
     
-    var visitor_count = document.getElementById("visitor_count");
+    var visitor_count = document.getElementById("body");
 visitor_count.value = data;
     
   
