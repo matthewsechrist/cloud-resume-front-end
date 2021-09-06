e63bad5feb4e7072dcbd55f4710183af797af64c
MattSechrist <80978245+MattSechrist@users.noreply.github.com>
2021-09-06 14:27:50 -0400

    Update api.js

--- public/css/api.js
+++ public/css/api.js
@@ -2,7 +2,7 @@ function updateCounter(){
 fetch("https://ivinlkop0h.execute-api.us-east-1.amazonaws.com/default/visitors")
   .then(response => response.text())
   .then((body) => {
-    document.getElementById("visitors").innerHTML=body
+    document.getElementById("visitor_counter").innerHTML=body
   })
   .catch(function(error) {
     console.log(error); 
