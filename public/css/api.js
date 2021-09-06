d0e6c1542c2f57d45b8f85d0dc948414abb7ad6f
MattSechrist <80978245+MattSechrist@users.noreply.github.com>
2021-09-06 14:11:31 -0400

    Update api.js

--- public/css/api.js
+++ public/css/api.js
@@ -1,29 +1,11 @@
-// api url
-const api_url = 
-    "https://ivinlkop0h.execute-api.us-east-1.amazonaws.com/default/visitors";
-  
-// Defining async function
-async function getapi(url) {
-    
-    // Storing response
-    const response = await fetch(url);
-    
-    // Storing data in form of JSON
-    var data = await response.json();
-    console.log(data);
-    if (response) {
-        hideloader();
-    }
-     document.getElementById("visitors").innerHTML = data.results;
-}
-// Calling that async function
-getapi(api_url);
-  
-// Function to hide the loader
-function hideloader() {
-    document.getElementById('loading').style.display = 'none';
-}
-// Function to define innerHTML for HTML table
+function updateCounter(){
+fetch("https://ivinlkop0h.execute-api.us-east-1.amazonaws.com/default/visitors")
+  .then(response => response.text())
+  .then((body) => {
+    document.getElementById("visitors").innerHTML=body
+  })
+  .catch(function(error) {
+    console.log(error); 
+  });  
+}  
 
-    // Setting innerHTML as tab variable
-    document.getElementById("visitors").innerHTML = data.results;
