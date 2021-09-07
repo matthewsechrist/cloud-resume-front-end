a7962cfe1573360d430f1b010fcf80039a99ea88
MattSechrist <matthew.sechrist@gmail.com>
2021-09-06 21:10:15 -0400

    Update api.js

--- public/js/api.js
+++ public/js/api.js
@@ -2,11 +2,9 @@ function updateCounter(){
 fetch("https://ivinlkop0h.execute-api.us-east-1.amazonaws.com/default/visitors")
   .then(response => response.text())
   .then((body) => {
-    document.getElementById("visitor_count").innerHTML=body
+    document.getElementById("visitor_count").innerHTML=body;
   })
   .catch(function(error) {
     console.log(error); 
   });  
 }  
-
-console.log(body);
