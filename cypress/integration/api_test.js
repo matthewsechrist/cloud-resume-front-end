326a7747805ffd7af8d59736d45576ee7746dfcd
MattSechrist <matthew.sechrist@gmail.com>
2021-09-08 17:07:21 -0400

    Update api_test.js

--- cypress/integration/api_test.js
+++ cypress/integration/api_test.js
@@ -1,5 +1,5 @@
-escribe('Visitor Test', () => {
+describe('Visitor Test', () => {
     it.only('Visit Visitor GET Test', () => {
-      cy.visit('https://ivinlkop0h.execute-api.us-east-1.amazonaws.com/default/visitors')
+      cy.request('https://ivinlkop0h.execute-api.us-east-1.amazonaws.com/default/visitors')
     })
   })
