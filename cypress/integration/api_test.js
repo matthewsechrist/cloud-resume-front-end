26d1cea796e904f1e4b352f298e98af750488ce8
MattSechrist <matthew.sechrist@gmail.com>
2021-09-08 21:38:31 -0400

    Updating Cypress tests

--- cypress/integration/api_test.js
+++ cypress/integration/api_test.js
@@ -1,5 +1,11 @@
 describe('Visitor Test', () => {
-    it.only('Visit Visitor GET Test', () => {
+    it.only('Visitor Test - GET Method', () => {
       cy.request('https://ivinlkop0h.execute-api.us-east-1.amazonaws.com/default/visitors')
+    })
+  })
+
+  describe('Index.html Test', () => {
+    it.only('Index.html Test', () => {
+      cy.request('https://matthewsechrist.cloud')
     })
   })
