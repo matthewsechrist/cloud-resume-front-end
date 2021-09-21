f563764bc83448c42c869b91d3f55675e9a1305b
MattSechrist <matthew.sechrist@gmail.com>
2021-09-20 22:25:21 -0400

    update uri

--- website_files/api.js
+++ website_files/api.js
@@ -1,5 +1,5 @@
 function visitorCounter(){
-fetch("https://ivinlkop0h.execute-api.us-east-1.amazonaws.com/default/visitors")
+fetch("https://2q4tyukeqb.execute-api.us-east-1.amazonaws.com")
   .then(response => response.text())
   .then((body) => {
     document.getElementById("visitor_count").innerHTML=body;
