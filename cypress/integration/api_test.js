11b19c93eca00653cb8850537d80046ee6902930
Matthew Sechrist <80978245+MattSechrist@users.noreply.github.com>
2021-09-26 13:33:17 -0400

    Added www domain test

--- cypress/integration/api_test.js
+++ cypress/integration/api_test.js
@@ -1,11 +1,17 @@
-describe('Visitor Test', () => {
+describe('API Gateway Test', () => {
     it.only('Visitor Test - GET Method', () => {
       cy.request('https://2q4tyukeqb.execute-api.us-east-1.amazonaws.com')
     })
   })
 
-  describe('Index.html Test', () => {
+  describe('Naked Domain Test', () => {
     it.only('Index.html Test', () => {
       cy.request('https://matthewsechrist.cloud')
+    })
+  })
+
+describe('www Domain Test', () => {
+    it.only('Index.html Test', () => {
+      cy.request('www.matthewsechrist.cloud')
     })
   })
