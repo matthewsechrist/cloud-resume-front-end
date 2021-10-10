dd8198fa49f6e04d1b7cf75fe0f4f68c5b3e694e
MattSechrist <matthew.sechrist@gmail.com>
2021-10-10 19:52:22 -0400

    website

--- website_files/api.js
+++ website_files/api.js
@@ -1,12 +1,38 @@
-function visitorCounter(){
-fetch("https://2q4tyukeqb.execute-api.us-east-1.amazonaws.com")
-  .then(response => response.text())
-  .then((body) => {
-    document.getElementById("visitor_count").innerHTML=body;
-  })
-  .catch(function(error) {
-    console.log(error); 
-  });  
-}  
+//function visitorCounter(){
+//fetch("https://2q4tyukeqb.execute-api.us-east-1.amazonaws.com")
+//  .then(response => response.text())
+//  .then((body) => {
+//    document.getElementById("visitor_count").innerHTML=body;
+//  })
+//  .catch(function(error) {
+//    console.log(error); 
+//  });  
+//}  
+//
+//visitorCounter();
 
-visitorCounter();
+function visitorCounter(){
+  fetch("https://ewufbufplbf7vnnjwn6mojnspq.appsync-api.us-east-1.amazonaws.com/graphql", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `query MyQuery {
+          visitorInfo(website_name: "") {
+            body
+          }
+        }        
+          `,
+      }),
+    })
+    .then(response => response.text())
+    .then((body) => {
+      document.getElementById("visitor_count").innerHTML=body;
+    })
+    .catch(function(error) {
+      console.log(error); 
+    });  
+  }  
+  //
+  visitorCounter();
