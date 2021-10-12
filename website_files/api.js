f4530ac994c86e00280537426765f9a7acf001fe
MattSechrist <matthew.sechrist@gmail.com>
2021-10-11 20:17:54 -0400

    Update api.js

--- website_files/api.js
+++ website_files/api.js
@@ -1,22 +1,9 @@
-//function visitorCounter(){
-//fetch("https://2q4tyukeqb.execute-api.us-east-1.amazonaws.com")
-//  .then(response => response.text())
-//  .then((body) => {
-//    document.getElementById("visitor_count").innerHTML=body;
-//  })
-//  .catch(function(error) {
-//    console.log(error); 
-//  });  
-//}  
-//
-//visitorCounter();
-
 function visitorCounter(){
-  fetch("https://ewufbufplbf7vnnjwn6mojnspq.appsync-api.us-east-1.amazonaws.com/graphql", {
+  fetch("https://ouvqczo3q5hohh6vyr5lmxgbhu.appsync-api.us-east-1.amazonaws.com/graphql", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/graphql',
-        'X-API-KEY': 'da2-aiszdcipufepxgs6532iaxnr6m'
+        'X-API-KEY': 'da2-puhkcdtr5nalldcr4qfc24xjqy'
       },
       body: JSON.stringify({
         query: `
