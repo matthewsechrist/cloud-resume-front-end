60bed70eb9fd77993fc579972a729bfe687c4934
MattSechrist <matthew.sechrist@gmail.com>
2021-10-14 22:23:39 -0400

    tf

--- website_files/api.js
+++ website_files/api.js
@@ -1,10 +1,10 @@
 function visitorCounter(){
 //  fetch("https://ouvqczo3q5hohh6vyr5lmxgbhu.appsync-api.us-east-1.amazonaws.com/graphql", {
-    fetch("https://switchfrenzy5i.korconnect.io/Visitor", {
+    fetch("https://switchfrenzy5i.korconnect.io/visitor", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/graphql',
-        'X-API-KEY': 'da2-puhkcdtr5nalldcr4qfc24xjqy'
+        'X-API-KEY': 'g1mKoexDQl1PSIrb5icjXmaGEtlo3IY8qjwkPDy5'
       },
       body: JSON.stringify({
         query: `
